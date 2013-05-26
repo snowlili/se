ca2cca522623fd8c6bee1ce28c3860bad3a0fb09
GX <gex9188@126.com>
2013-05-26 13:39:22 +0800

    ver0.4
    完成了70%

--- Lib/Action/TransactionAction.class.php
+++ Lib/Action/TransactionAction.class.php
@@ -1,6 +1,22 @@
 <?php
 class TransactionAction extends Action {
 	public function index(){
+        $Data = M('transaction'); // 实例化Data数据模型
+		$Transaction = $Data->select();
+		if($Transaction) {
+			$this->transactions = $Transaction;// 模板变量赋值
+			//dump($this->transaction);
+		}else{
+			$this->error('数据错误');
+		}
+		if( $_POST ) 
+		{ 
+			$abc = $_POST['checktype']; 
+			trace($abc);
+		} 
+		$this->checktype[1]=true;
+		trace($this->checktype[1]);
+		trace($array);
         $this->display();
     }
 }
